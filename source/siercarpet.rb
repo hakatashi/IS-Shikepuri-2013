0ea8268a9a67acbf42e9c0fbb6c47240df33159a
hakatashi <hakatasiloving@gmail.com>
2014-02-10 20:17:21 +0900

    不要なfill操作を削除

--- source/siercarpet.rb
+++ source/siercarpet.rb
@@ -14,12 +14,6 @@ def subsiercarpet(a, n, x, y)
       for carpY in 0..2 do
         if carpX != 1 || carpY != 1
           subsiercarpet(a, n - 1, x + carpX * 3 ** (n-1), y + carpY * 3 ** (n-1))
-        else
-          for fillX in 0..3**(n-1)-1 do
-            for fillY in 0..3**(n-1)-1 do
-              a[x + carpX * 3 ** (n-1) + fillX][y + carpY * 3 ** (n-1) + fillY] = 0
-            end
-          end
         end
       end
     end
