63e22634ee905ca658ddbc766734a3248b10f6b8
hakatashi <hakatasiloving@gmail.com>
2014-02-10 20:31:40 +0900

    コメントを追加

--- source/siercarpet.rb
+++ source/siercarpet.rb
@@ -1,3 +1,5 @@
+# カントール集合のプログラムの焼き直しです。
+
 load("./make2d.rb")
 
 def siercarpet(n)
@@ -6,13 +8,20 @@ def siercarpet(n)
   show(a)
 end
 
+# a という配列に (x, y) を左上隅とする n 次のシェルピンスキーのカーペットを生成する関数
 def subsiercarpet(a, n, x, y)
+  # 0次なら単純に一個塗りつぶすだけ
   if n == 0
     a[x][y] = 1
-  else
+  else # 0次でないなら、いっこ下の次元のサブカーペットを呼び出す
+    # 3x3のサブカーペットが必要なので、x方向とy方向にその回数ずつだけループさせる
     for carpX in 0..2 do
       for carpY in 0..2 do
+        # まんなかの一個を除く
         if carpX != 1 || carpY != 1
+          # サブカーペットを帰納的に呼び出します。
+          # 左上隅の計算部分がちょっとややこしいかもしれません。
+          # なお、rubyでは乗算(*)より冪乗(**)が優先されます。
           subsiercarpet(a, n - 1, x + carpX * 3 ** (n-1), y + carpY * 3 ** (n-1))
         end
       end
--- source/sierpinski.rb
+++ source/sierpinski.rb
@@ -1,3 +1,5 @@
+# スライド資料の組み合わせプログラムとほぼ同じです。
+
 load("./make2d.rb")
 
 def sierpinski(n)
@@ -6,6 +8,10 @@ def sierpinski(n)
   for x in 1..n-1
     a[x][0] = 1
     for y in 1..(x-1)
+      # x-1Cy-1 の余りと x-1Cy の余りを足して、 xCy を2で割った余りを求める。
+      # modの性質によりそのまま加算できる。
+      # a[x][y] = (a[x-1][y-1] + a[x-1][y]) % 2
+      # などでもいいが、xorを使うとシンプルに書ける。
       a[x][y] = a[x-1][y-1] ^ a[x-1][y]
     end
     a[x][x] = 1
